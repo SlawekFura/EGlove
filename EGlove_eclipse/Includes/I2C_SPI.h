dc2b76190811835619882c2a21cf47e0210d4e03
Slawek <slawek.fura@gmail.com>
2016-10-08 02:51:19 +0200

    Import EWork + Receiver.sln

---
+++ EGlove_eclipse/Includes/I2C_SPI.h
@@ -0,0 +1,58 @@
+/*
+ * I2C_SPI.h
+ *
+ *  Created on: 26 sie 2016
+ *      Author: Mariusz
+ */
+
+#ifndef I2C_SPI_H_
+#include "stm32f4xx_hal.h"
+
+#define I2C_SPI_H_
+
+
+#define LSM303_ACC_ADDRESS (0x19 << 1) // adres akcelerometru: 0011 001x
+#define LSM303_ACC_CTRL_REG1_A 0x20 // rejestr ustawien 1
+#define LSM303_ACC_XYZ_ENABLE 0x07 // 0000 0100
+#define LSM303_ACC_100HZ 0x50 // 0101 0000
+#define LSM303_ACC_X_H 0x29 // wyzszy bajt danych osi X
+#define LSM303_ACC_X_L 0x28 // nizszy bajt danych osi X
+#define LSM303_ACC_Y_H 0x2B // wyzszy bajt danych osi Y
+#define LSM303_ACC_Y_L 0x2A // nizszy bajt danych osi Y
+#define LSM303_ACC_Z_H 0x2D // wyzszy bajt danych osi Z
+#define LSM303_ACC_Z_L 0x2C // nizszy bajt danych osi Z
+#define LSM303_ACC_RESOLUTION 2
+uint8_t AccSettings;
+
+#define L3GD20_GYRO_CTRL_REG1 0x20 // rejestr ustawien 1
+#define L3GD20_GYRO_190HZ_25BW 0x50 // 0101 0000
+#define L3GD20_GYRO_ENABLE 0x08 // 0000 1000
+#define L3GD20_GYRO_ZXY_ENABLE 0x07 // 0000 0100
+#define L3GD20_GYRO_MS_BIT 0x40
+#define L3GD20_GYRO_X_H 0x29 // wyzszy bajt danych osi Z
+#define L3GD20_GYRO_X_L 0x28 // wyzszy bajt danych osi Z
+#define L3GD20_GYRO_Y_H 0x2B // wyzszy bajt danych osi Z
+#define L3GD20_GYRO_Y_L 0x2A // wyzszy bajt danych osi Z
+#define L3GD20_GYRO_Z_H 0x2D // wyzszy bajt danych osi Z
+#define L3GD20_GYRO_Z_L 0x2C // wyzszy bajt danych osi Z
+#define L3GD20_GYRO_WHO_AM_I 0x0F
+
+
+uint8_t sendToGyro[9];
+uint8_t * pSendSPI[9];
+
+HAL_StatusTypeDef initAccI2C(I2C_HandleTypeDef * hi2c);
+HAL_StatusTypeDef initGyroSPI(SPI_HandleTypeDef *hspi);
+
+void getPositionDataACC(I2C_HandleTypeDef *hi2c, double *pDataGetXAxis,
+		double *pDataGetYAxis,  double *pDataGetZAxis, uint32_t Timeout);
+
+void getPositionDataSPI(SPI_HandleTypeDef *hspi, double *pDataGetXAxis,
+		double *pDataGetYAxis,  double *pDataGetZAxis, uint32_t Timeout);
+
+void fillDataToSend(char *arr,int arrSize,const float gyroX,const float gyroY,
+		const float accX,const float accY);
+
+
+
+#endif /* I2C_SPI_H_ */
---
+++ EGlove_eclipse/Sources/I2C_SPI.c
@@ -0,0 +1,190 @@
+/*
+ * I2C_SPI.c
+ *
+ *  Created on: 26 sie 2016
+ *      Author: Mariusz
+ */
+#include "I2C_SPI.h"
+#include <math.h>
+#include <limits.h>
+#include <stdlib.h>
+
+
+uint8_t AccSettings = LSM303_ACC_XYZ_ENABLE | LSM303_ACC_100HZ;
+
+enum {GyroSettings = L3GD20_GYRO_190HZ_25BW | L3GD20_GYRO_ENABLE | L3GD20_GYRO_ZXY_ENABLE};
+uint8_t sendToGyro[9]= {L3GD20_GYRO_CTRL_REG1 , GyroSettings,
+		L3GD20_GYRO_X_L | 128 | L3GD20_GYRO_MS_BIT,L3GD20_GYRO_X_H | 128, //multi reading
+		L3GD20_GYRO_Y_L | 128,L3GD20_GYRO_Y_H | 128,
+		L3GD20_GYRO_Z_L | 128,L3GD20_GYRO_Z_H | 128,
+		L3GD20_GYRO_WHO_AM_I | 128};
+uint8_t * pSendSPI[9] = {sendToGyro, &sendToGyro[1], &sendToGyro[2],
+		&sendToGyro[3], &sendToGyro[4], &sendToGyro[5],
+		&sendToGyro[6], &sendToGyro[7], &sendToGyro[8]};
+
+HAL_StatusTypeDef initAccI2C(I2C_HandleTypeDef * hi2c){
+	  return HAL_I2C_Mem_Write(hi2c, LSM303_ACC_ADDRESS, LSM303_ACC_CTRL_REG1_A, 1, &AccSettings, 1, 100);
+}
+HAL_StatusTypeDef initGyroSPI(SPI_HandleTypeDef *hspi){
+	HAL_StatusTypeDef status;
+	HAL_GPIO_WritePin(GPIOE,GPIO_PIN_3,RESET);
+	if((status = HAL_SPI_Transmit(hspi,*pSendSPI,1,100))!= HAL_OK){
+		HAL_GPIO_WritePin(GPIOE,GPIO_PIN_3,SET);
+		return status;
+	}
+
+	if((status = HAL_SPI_Transmit(hspi,*pSendSPI + 1,1,100))!= HAL_OK){
+		HAL_GPIO_WritePin(GPIOE,GPIO_PIN_3,SET);
+		return status;
+	}
+	HAL_GPIO_WritePin(GPIOE,GPIO_PIN_3,SET);
+	return HAL_ERROR;
+}
+
+void getPositionDataACC(I2C_HandleTypeDef *hi2c, double *pDataGetXAxis,
+		double *pDataGetYAxis,  double *pDataGetZAxis, uint32_t Timeout){
+
+	uint8_t tempLow = 0, tempHigh = 0;
+	int16_t tempInt = 0;
+
+	 HAL_I2C_Mem_Read(hi2c, LSM303_ACC_ADDRESS, LSM303_ACC_X_H, 1, &tempHigh, 1, 100);
+	 HAL_I2C_Mem_Read(hi2c, LSM303_ACC_ADDRESS, LSM303_ACC_X_L, 1, &tempLow, 1, 100);
+	 tempInt = (tempHigh << 8) | tempLow;
+	 if((float)tempInt/(INT16_MAX)*2>1)
+	 	*pDataGetYAxis = asin(1);
+	 else if((float)tempInt/(INT16_MAX)*2<-1)
+	 	*pDataGetYAxis = asin(-1);
+	 else
+	 	*pDataGetYAxis = (asin((float)tempInt/(INT16_MAX)*2));
+
+
+
+	 HAL_I2C_Mem_Read(hi2c, LSM303_ACC_ADDRESS, LSM303_ACC_Y_H, 1, &tempHigh, 1, 100);
+	 HAL_I2C_Mem_Read(hi2c, LSM303_ACC_ADDRESS, LSM303_ACC_Y_L, 1, &tempLow, 1, 100);
+	 tempInt = (tempHigh << 8) | tempLow;
+	 if((float)tempInt/(INT16_MAX)*2>1)
+	 	*pDataGetXAxis = asin(-1);
+	 else if((float)tempInt/(INT16_MAX)*2<-1)
+	 	*pDataGetXAxis = asin(1);
+	 else
+	 	*pDataGetXAxis = -(asin((float)tempInt/(INT16_MAX)*2));
+
+	 HAL_I2C_Mem_Read(hi2c, LSM303_ACC_ADDRESS, LSM303_ACC_Z_H, 1, &tempHigh, 1, 100);
+	 HAL_I2C_Mem_Read(hi2c, LSM303_ACC_ADDRESS, LSM303_ACC_Z_L, 1, &tempLow, 1, 100);
+	 tempInt = (tempHigh << 8) | tempLow;
+	 if((float)tempInt/(INT16_MAX)*2>1)
+	 	*pDataGetZAxis = asin(-1);
+	 else if((float)tempInt/(INT16_MAX)*2<-1)
+	 	*pDataGetZAxis = asin(1);
+	 else
+	 	*pDataGetZAxis = -(asin((float)tempInt/(INT16_MAX)*2));
+
+
+}
+
+void getPositionDataSPI(SPI_HandleTypeDef *hspi,  double * pDataGetXAxis,
+		double *pDataGetYAxis,  double *pDataGetZAxis,uint32_t Timeout){
+	uint8_t DataGetAxisTemp1;
+	int16_t DataGetAxisTemp2;
+	uint8_t *pDataGetAxisTemp1 = &DataGetAxisTemp1;
+	int16_t *pDataGetAxisTemp2 = &DataGetAxisTemp2;
+
+	HAL_GPIO_WritePin(GPIOE,GPIO_PIN_3,RESET);
+	HAL_SPI_Transmit(hspi,*pSendSPI + 2,1,100);
+
+	HAL_SPI_Receive(hspi,pDataGetAxisTemp1,1,100);
+	HAL_SPI_Receive(hspi,pDataGetAxisTemp2,1,100);
+	*pDataGetAxisTemp2 = ((*pDataGetAxisTemp2 << 8) /*| (*pDataGetAxisTemp1)*/);
+	*pDataGetXAxis =(double)(*pDataGetAxisTemp2)/130.55*3.1415/180 +0.034;///INT16_MAX;//36/25/2/3.1415;
+
+	HAL_SPI_Receive(hspi,pDataGetAxisTemp1,1,100);
+	HAL_SPI_Receive(hspi,pDataGetAxisTemp2,1,100);
+	*pDataGetAxisTemp2 = ((*pDataGetAxisTemp2 << 8) | (*pDataGetAxisTemp1));
+	*pDataGetYAxis = (double)*pDataGetAxisTemp2/INT16_MAX*2*3.1415;
+
+	HAL_SPI_Receive(hspi,pDataGetAxisTemp1,1,100);
+	HAL_SPI_Receive(hspi,pDataGetAxisTemp2,1,100);
+	*pDataGetAxisTemp2 = ((*pDataGetAxisTemp2 << 8) | (*pDataGetAxisTemp1));
+	*pDataGetZAxis = (double)*pDataGetAxisTemp2/INT16_MAX*2*3.1415;
+
+	HAL_GPIO_WritePin(GPIOE,GPIO_PIN_3,SET);
+}
+
+void fillDataToSend(char *arr,int arrSize,const float gyroX,const float gyroY,
+		const float accX,const float accY){
+	for(int i = 0;;){
+		if(i>=arrSize)
+			break;
+		if(i==0)arr[i]='G';
+		if(i==1)arr[i]='X';
+		if(i==6)arr[i]='Y';
+		if(i==11)arr[i]='A';
+		if(i==12)arr[i]='X';
+		if(i==17)arr[i]='Y';
+		i++;
+		if(i==2)
+			for(int j=0;j<4;j++){
+				int m = j;
+				if(j>0&gyroX<0)
+					m--;
+				int k = (int)(gyroX*pow(10,m))%10;
+				if(j==0&&gyroX<0){
+					arr[i]='-';
+					itoa(abs(k),arr+i+1,10);
+					i++;j++;
+				}
+				else
+					itoa(abs(k),arr+i,10);
+				i++;
+			};
+		if(i==7)
+			for(int j=0;j<4;j++){
+				int m = j;
+				if(j>0&&gyroY<0)
+					m--;
+				int k = (int)(gyroY*pow(10,m))%10;
+				if(j==0&&gyroY<0){
+					arr[i]='-';
+					itoa(abs(k),arr+i+1,10);
+					i++;j++;
+				}
+				else
+					itoa(abs(k),arr+i,10);
+				i++;
+			};
+		if(i==13)
+			for(int j=0;j<4;j++){
+				int m = j;
+				if(j>0&&accX<0)
+					m--;
+				int k = (int)(accX*pow(10,m))%10;
+				if(j==0&&accX<0){
+					arr[i]='-';
+					itoa(abs(k),arr+i+1,10);
+					i++;j++;
+				}
+				else
+					itoa(abs(k),arr+i,10);
+				i++;
+			};
+		if(i==18)
+			for(int j=0;j<4;j++){
+				int m = j;
+				if(j>0&&accY<0)
+					m--;
+				int k = (int)(accY*pow(10,m))%10;
+				if(j==0&&accY<0){
+					arr[i]='-';
+					itoa(abs(k),arr+i+1,10);
+					i++;j++;
+				}
+				else
+					itoa(abs(k),arr+i,10);
+				i++;
+			};
+
+	}
+}
+
+
+
